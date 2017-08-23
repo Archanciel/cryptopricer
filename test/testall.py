ce579144cf81b6a7c314b0f75dbe01db6acd93d8
Archanciel <jp.schnyder@gmail.com>
2017-08-23 17:56:25 +0200

    add test abstract command

--- test/testall.py
+++ test/testall.py
@@ -1,10 +1,12 @@
 from unittest import TestLoader, TextTestRunner, TestSuite 
 
 from testrequester import TestRequester
+from testabstractcommand import TestAbstractCommand
 
 if __name__ == "__main__":
     loader = TestLoader() 
     suite = TestSuite((loader.loadTestsFromTestCase(TestRequester),
+                       loader.loadTestsFromTestCase(TestAbstractCommand)
                        ))
     runner = TextTestRunner(verbosity = 2) 
     runner.run(suite)
