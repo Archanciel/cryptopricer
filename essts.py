d537c94249c99a29d1ab9c93c848ebf7c6b52863
Archanciel <jp.schnyder@gmail.com>
2017-09-03 01:32:37 +0200

    initial commit

---
+++ essts.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from pytz import timezone
+import time
+
+def timestamp2date(timestamp):
+    DATE_TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+    # function converts a UTC timestamp into Europe/Zurich Gregorian date
+    utcTimeStamp = datetime.fromtimestamp(int(timestamp)).replace(tzinfo=timezone('UTC'))
+
+    return utcTimeStamp.astimezone(timezone('Europe/Zurich')).strftime(DATE_TIME_FORMAT)
+
+timeStampUTC_1 = time.mktime(datetime.utcnow().timetuple())
+print(timeStampUTC_1)
+print(timestamp2date(timeStampUTC_1))
+timeStampUTC_2 = time.mktime(datetime.now(timezone('UTC')).timetuple())
+print(timeStampUTC_2)
+print(timestamp2date(timeStampUTC_2))
+print(timeStampUTC_2 - timeStampUTC_1)
+
+
+# 1504385450.0
+# 2017-09-03 00:50:50   this the right time
+# 1504389050.0
+# 2017-09-03 01:50:50
+# 3600.0
