77ce9b6de3f78ba81d2d12feacbeb905e83333ec
Archanciel <jp.schnyder@gmail.com>
2017-09-03 01:33:01 +0200

    Now working

--- fetchHistBtcPrice.py
+++ fetchHistBtcPrice.py
@@ -4,57 +4,52 @@ import json
 from bs4 import BeautifulSoup
 import urllib.request
 from tqdm import tqdm
+from pytz import timezone
 
 def timestamp2date(timestamp):
-    # function converts a Uniloc timestamp into Gregorian date
-    return datetime.fromtimestamp(int(timestamp)).strftime('%Y-%m-%d')
+    # function converts a UTC timestamp into Europe/Zurich Gregorian date
+    utcTimeStamp = datetime.fromtimestamp(int(timestamp)).replace(tzinfo=timezone('UTC'))
 
-def date2timestamp(date):
-    # function coverts Gregorian date in a given format to timestamp
-    return datetime.strptime(date, '%Y-%m-%d').timestamp()
+    return utcTimeStamp.astimezone(timezone('Europe/Zurich')).strftime('%Y-%m-%d %H:%M:%S')
 
 def fetchCryptoOHLC(fsym, tsym):
     # function fetches a crypto price-series for fsym/tsym and stores
     # it in pandas DataFrame
 
-    cols = ['date', 'timestamp', 'open', 'high', 'low', 'close']
     lst = ['time', 'open', 'high', 'low', 'close']
 
-    #timestamp = time.mktime(today().timetuple())
-    timestamp = time.mktime(datetime(2016, 8, 7).timetuple())
+    timestamp_ = time.mktime(datetime.utcnow().timetuple())
+    print(timestamp_)
+    timestamp = time.mktime(datetime.now(timezone('UTC')).timetuple())
+    print(timestamp)
 
-
-    # (limit-1) * 2 = days
-    # One year is around 184
-    limit = 100 
-    url = "https://min-api.cryptocompare.com/data/histoday?fsym=" + fsym + "&tsym=" + tsym + "&toTs=" + str(int(timestamp)) + "&limit=" + str(limit)
+    limit = 3
+    url = "https://min-api.cryptocompare.com/data/histominute?fsym=" + fsym + "&tsym=" + tsym + "&toTs=" + str(int(timestamp)) + "&limit=" + str(limit)
     page = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(page, 'html.parser')
     dic = json.loads(soup.prettify())
+
     for i in range(0, limit):
-        tmp = []
+        tmp = [fsym + '/' + tsym]
         for e in enumerate(lst):
             x = e[0]
             y = dic['Data'][i][e[1]]
             if(x == 0):
                 tmp.append(str(timestamp2date(y)))
-            tmp.append(y)
+            else:
+                tmp.append(y)
+        print('')
         print(tmp)
-        print('/n')
 
 def get_multiple_cryptos(symbols):
 
     # Adding columns with data for all requested cryptocurrencies
     for symbol in tqdm(symbols):
         fsym = symbol
-        tsym = "BTC"
-        data_symbol = fetchCryptoOHLC(fsym, tsym)
-
-        print(data_symbol )
-
+        tsym = "USD"
+        fetchCryptoOHLC(fsym, tsym)
 
 if __name__ == '__main__':
-
-    symbols = ['ETH', 'LTC', 'ETC', 'DOGE', 'DGB', 'SC']
+    symbols = ['BTC', 'ETH']
     get_multiple_cryptos(symbols)
     #fetchCryptoOHLC('BTC', 'USD')
--- pricerequester.py
+++ pricerequester.py
@@ -1,13 +1,61 @@
-import datetime
+from datetime import datetime
 from pytz import timezone
+import time
+import json
+import urllib.request
+from urllib.error import HTTPError, URLError
+import sys
+from bs4 import BeautifulSoup
+#from tqdm import tqdm
+
+DATE_TIME_FORMAT = "%d-%m-%Y %H:%M:%S"
+DATE_TIME_FORMAT_TZ = DATE_TIME_FORMAT + " %Z%z"
+
 
 class PriceRequester:
+    def __init__(self, localTimeZone):
+        self.localTimeZone = localTimeZone
+
+    def _timestamp2date(self, timestamp):
+        # function converts a UTC timestamp into Europe/Zurich Gregorian date
+        utcTimeStamp = datetime.fromtimestamp(int(timestamp - 3600)).replace(tzinfo=timezone('UTC'))
+
+        return utcTimeStamp.astimezone(timezone(self.localTimeZone)).strftime(DATE_TIME_FORMAT)
+
+    def getPriceAtDatetime(self, coin, fiat, timeStampUTC, exchange):
+        timeStamp = str(int(timeStampUTC + 60))
+        url = "https://min-api.cryptocompare.com/data/histominute?fsym={}&tsym={}&limit=1&aggregate=1&toTs={}".format(coin, fiat, timeStamp)
+        tmp = []
+
+        try:
+            webURL = urllib.request.urlopen(url)
+        except HTTPError as e:
+            sys.exit('Could not complete request. Reason: ' + e.reason)
+        except URLError as e:
+            sys.exit('Could not complete request. Reason: ' + e.reason)
+        else:
+            page = webURL.read()
+            soup = BeautifulSoup(page, 'html.parser')
+            dic = json.loads(soup.prettify())
+#            print(dic)
+            lst = ['time', 'open', 'high', 'low', 'close']
+            for e in enumerate(lst):
+                x = e[0]
+                y = dic['Data'][0][e[1]]
+                if (x == 0):
+                    tmp.append(str(self._timestamp2date(y)))
+                else:
+                    tmp.append(y)
+
+        return tmp
+
     def explore(self):
         print('\nConverting now ...')
         fmt = "%Y-%m-%d %H:%M:%S %Z%z"
 
         print('Current time in UTC')
-        now_utc = datetime.datetime.now(timezone('UTC'))
+        now_utc = datetime.now(timezone('UTC'))
+#        now_utc = datetime.utcnow()
         print(now_utc.strftime(fmt))
 
         print('Convert to US/Pacific time zone')
@@ -20,32 +68,51 @@ class PriceRequester:
 
 
         print('\nLocalizing a date/time')
-        date_str = "2014-05-28 22:28:15"
-        datetime_obj_naive = datetime.datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
+        dateStr = "2014-05-28 22:28:15"
+        datetimeObj_naive = datetime.strptime(dateStr, "%Y-%m-%d %H:%M:%S")
 
         # Wrong way!
-        datetime_obj_pacific = datetime_obj_naive.replace(tzinfo=timezone('US/Pacific'))
+        datetimeObj_pacific = datetimeObj_naive.replace(tzinfo=timezone('US/Pacific'))
         print('WRONG')
-        print(datetime_obj_pacific.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
+        print(datetimeObj_pacific.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
 
         # Right way!
-        datetime_obj_pacific = timezone('US/Pacific').localize(datetime_obj_naive)
+        datetimeObj_pacific = timezone('US/Pacific').localize(datetimeObj_naive)
         print('RIGHT')
-        print(datetime_obj_pacific.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
+        print(datetimeObj_pacific.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
 
 
         print('\nConverting UTC date to other time zone')
-        date_str = "2014-05-28 22:28:15"
-        datetime_obj = datetime.datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
-        datetime_obj_utc = datetime_obj.replace(tzinfo=timezone('UTC'))
+        dateStr = "2014-05-28 22:28:15"
+        datetimeObj = datetime.strptime(dateStr, "%Y-%m-%d %H:%M:%S")
+        datetimeObjUTC = datetimeObj.replace(tzinfo=timezone('UTC'))
         print('Date in UTC')
-        print(datetime_obj_utc.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
+        print(datetimeObjUTC.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
         print('Convert to US/Pacific time zone')
-        pacificTime = datetime_obj_utc.astimezone(timezone('US/Pacific'))
+        pacificTime = datetimeObjUTC.astimezone(timezone('US/Pacific'))
         print(pacificTime.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
         print('Convert Europe/Zurich time zone')
-        zurichTime = datetime_obj_utc.astimezone(timezone('Europe/Zurich'))
+        zurichTime = datetimeObjUTC.astimezone(timezone('Europe/Zurich'))
         print(zurichTime.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
 
-pr = PriceRequester()
-pr.explore()
+pr = PriceRequester('Europe/Zurich')
+#pr.explore()
+dateStr = "28-08-2017 12:29:01"
+datetimeObj = datetime.strptime(dateStr, DATE_TIME_FORMAT)
+datetimeObjUTC = datetimeObj.replace(tzinfo=timezone('UTC'))
+print('Date in UTC')
+print(datetimeObjUTC.strftime(DATE_TIME_FORMAT_TZ))
+timeStampUTC = time.mktime(datetimeObjUTC.timetuple())
+print(timeStampUTC)
+
+datetimeObjZH = datetimeObjUTC.astimezone(timezone('Europe/Zurich'))
+print('Date in Europe/Zurich')
+print(datetimeObjZH.strftime(DATE_TIME_FORMAT_TZ))
+timeStampZH = time.mktime(datetimeObjZH.timetuple())
+print(timeStampZH)
+print(timeStampZH - timeStampUTC)
+print(pr.getPriceAtDatetime('BTC', 'USD', timeStampUTC, 'CCEX'))
+
+#timeStampUTC = time.mktime(datetime.utcnow().timetuple())
+timeStampUTC = time.mktime(datetime.now(timezone('UTC')).timetuple())
+print(pr.getPriceAtDatetime('BTC', 'USD', timeStampUTC, 'CCEX'))
