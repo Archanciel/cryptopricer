6235e664b23d8cb87aecdade9597bf068955a5b9
Archanciel <jp.schnyder@gmail.com>
2017-09-03 04:19:38 +0200

    add getPriceAtLocalDateTime

--- pricerequester.py
+++ pricerequester.py
@@ -8,7 +8,7 @@ import sys
 from bs4 import BeautifulSoup
 #from tqdm import tqdm
 
-DATE_TIME_FORMAT = "%d-%m-%Y %H:%M:%S"
+DATE_TIME_FORMAT = "%d-%m-%y %H:%M"
 DATE_TIME_FORMAT_TZ = DATE_TIME_FORMAT + " %Z%z"
 
 
@@ -22,7 +22,15 @@ class PriceRequester:
 
         return utcTimeStamp.astimezone(timezone(self.localTimeZone)).strftime(DATE_TIME_FORMAT)
 
-    def getPriceAtDatetime(self, coin, fiat, timeStampUTC, exchange):
+    def getPriceAtLocalDateTime(self, coin, fiat, localDateTimeStr, exchange):
+        datetimeObj = datetime.strptime(localDateTimeStr, DATE_TIME_FORMAT)
+        localDatetimeObj = timezone(self.localTimeZone).localize(datetimeObj)
+        datetimeObjUTC = localDatetimeObj.astimezone(timezone('UTC'))
+        timeStampUTC = time.mktime(datetimeObjUTC.timetuple())
+
+        return self.getPriceAtTimeStampUTC(coin, fiat, timeStampUTC, exchange)
+
+    def getPriceAtTimeStampUTC(self, coin, fiat, timeStampUTC, exchange):
         timeStamp = str(int(timeStampUTC + 60))
         url = "https://min-api.cryptocompare.com/data/histominute?fsym={}&tsym={}&limit=1&aggregate=1&toTs={}".format(coin, fiat, timeStamp)
         tmp = []
@@ -37,11 +45,12 @@ class PriceRequester:
             page = webURL.read()
             soup = BeautifulSoup(page, 'html.parser')
             dic = json.loads(soup.prettify())
-#            print(dic)
+            dataDic = dic['Data'][0]
+#            print(dataDic)
             lst = ['time', 'open', 'high', 'low', 'close']
             for e in enumerate(lst):
                 x = e[0]
-                y = dic['Data'][0][e[1]]
+                y = dataDic[e[1]]
                 if (x == 0):
                     tmp.append(str(self._timestamp2date(y)))
                 else:
@@ -81,6 +90,15 @@ class PriceRequester:
         print('RIGHT')
         print(datetimeObj_pacific.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
 
+        datetimeObj = datetime.strptime(dateStr, "%Y-%m-%d %H:%M:%S")
+        localDatetimeObj = timezone("Europe/Zurich").localize(datetimeObj)
+        print("local time:               " + localDatetimeObj.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
+        datetimeObjUTC = localDatetimeObj.astimezone(timezone('UTC'))
+        timeStampUTC = time.mktime(datetimeObjUTC.timetuple())
+        print("local time to UTC time:   " + datetimeObjUTC.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
+        swissDateTimeObj = datetimeObjUTC.astimezone(timezone("Europe/Zurich"))
+        print("local time from UTC time: " + swissDateTimeObj.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
+
 
         print('\nConverting UTC date to other time zone')
         dateStr = "2014-05-28 22:28:15"
@@ -95,24 +113,49 @@ class PriceRequester:
         zurichTime = datetimeObjUTC.astimezone(timezone('Europe/Zurich'))
         print(zurichTime.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
 
-pr = PriceRequester('Europe/Zurich')
-#pr.explore()
-dateStr = "28-08-2017 12:29:01"
-datetimeObj = datetime.strptime(dateStr, DATE_TIME_FORMAT)
-datetimeObjUTC = datetimeObj.replace(tzinfo=timezone('UTC'))
-print('Date in UTC')
-print(datetimeObjUTC.strftime(DATE_TIME_FORMAT_TZ))
-timeStampUTC = time.mktime(datetimeObjUTC.timetuple())
-print(timeStampUTC)
+def play():
+    pr = PriceRequester('Europe/Zurich')
+    pr.explore()
+    localDateTimeStr = "28-08-17 12:29"
+    datetimeObj = datetime.strptime(localDateTimeStr, DATE_TIME_FORMAT)
+    datetimeObjUTC = datetimeObj.replace(tzinfo=timezone('UTC'))
+    print('Date in UTC')
+    print(datetimeObjUTC.strftime(DATE_TIME_FORMAT_TZ))
+    timeStampUTC = time.mktime(datetimeObjUTC.timetuple())
+    print(timeStampUTC)
 
-datetimeObjZH = datetimeObjUTC.astimezone(timezone('Europe/Zurich'))
-print('Date in Europe/Zurich')
-print(datetimeObjZH.strftime(DATE_TIME_FORMAT_TZ))
-timeStampZH = time.mktime(datetimeObjZH.timetuple())
-print(timeStampZH)
-print(timeStampZH - timeStampUTC)
-print(pr.getPriceAtDatetime('BTC', 'USD', timeStampUTC, 'CCEX'))
+    datetimeObjZH = datetimeObjUTC.astimezone(timezone('Europe/Zurich'))
+    print('Date in Europe/Zurich')
+    print(datetimeObjZH.strftime(DATE_TIME_FORMAT_TZ))
+    timeStampZH = time.mktime(datetimeObjZH.timetuple())
+    print(timeStampZH)
+    print(timeStampZH - timeStampUTC)
+    crypto = 'BTC'
+    fiat = 'USD'
+    print(crypto + ' ' + str(pr.getPriceAtTimeStampUTC(crypto, fiat, timeStampUTC, 'CCEX')))
 
-#timeStampUTC = time.mktime(datetime.utcnow().timetuple())
-timeStampUTC = time.mktime(datetime.now(timezone('UTC')).timetuple())
-print(pr.getPriceAtDatetime('BTC', 'USD', timeStampUTC, 'CCEX'))
+    #timeStampUTC = time.mktime(datetime.utcnow().timetuple())
+    timeStampUTC = time.mktime(datetime.now(timezone('UTC')).timetuple())
+    # crypto = 'BTC'
+    # fiat = 'USD'
+    # print(crypto + ' ' + str(pr.getPriceAtTimeStampUTC(crypto, fiat, timeStampUTC, 'CCEX')))
+    crypto = 'MCAP'
+    print(crypto + ' ' + str(pr.getPriceAtTimeStampUTC(crypto, fiat, timeStampUTC, 'CCEX')))
+
+    localDateTimeStr = "1-9-17 11:00"
+    print(crypto + ' ' + str(pr.getPriceAtLocalDateTime(crypto, fiat, localDateTimeStr, 'CCEX')))
+
+if __name__ == '__main__':
+    play()
+    import re
+    pr = PriceRequester('Europe/Zurich')
+    inputStr = input("MCAP USD 1-9-17 11:00 (q/quit): ")
+
+    while inputStr.upper() != 'Q':
+        pattern = r"(\w+) (\w+) ([0-9\-]+) ([0-9:]+)"
+        data = re.match(pattern, inputStr.upper())
+        crypto = data.group(1)
+        fiat = data.group(2)
+        localDateTimeStr = data.group(3) + ' ' + data.group(4)
+        print(crypto + '/' + fiat + ': ' + ' '.join(map(str, pr.getPriceAtLocalDateTime(crypto, fiat, localDateTimeStr, 'CCEX'))))
+        inputStr = input("MCAP USD 1-9-17 11:00 (q to quit): ")
