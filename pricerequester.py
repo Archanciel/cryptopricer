31907cabc1e91552b2ee7b56b2cf54a464754b05
Archanciel <jp.schnyder@gmail.com>
2017-09-10 19:36:35 +0200

    Date format change + enter new optional values in loop

--- pricerequester.py
+++ pricerequester.py
@@ -8,7 +8,7 @@ import sys
 from bs4 import BeautifulSoup
 #from tqdm import tqdm
 
-DATE_TIME_FORMAT = "%d-%m-%y %H:%M"
+DATE_TIME_FORMAT = "%d/%m/%y %H:%M"
 DATE_TIME_FORMAT_TZ = DATE_TIME_FORMAT + " %Z%z"
 
 
@@ -35,7 +35,6 @@ class PriceRequester:
 
     def getPriceAtUTCTimeStamp(self, coin, fiat, timeStampUTC, exchange):
         timeStamp = str(int(timeStampUTC + 60))
-        #timeStamp = str(int(timeStampUTC + 45))
         url = "https://min-api.cryptocompare.com/data/histominute?fsym={}&tsym={}&limit=1&aggregate=1&toTs={}&e={}".format(coin, fiat, timeStamp, exchange)
         tmp = []
 
@@ -54,157 +53,82 @@ class PriceRequester:
             dic = json.loads(soup.prettify())
             if dic['Data'] != []:
                 dataDic = dic['Data'][0]
-    #            print(dataDic)
                 lst = ['time', 'open', 'high', 'low', 'close']
                 tmp.append(str(self._UTCTimestamp2LocalizedDate(dataDic['time'])))
                 tmp.append(dataDic['close'])
-                """
-                for e in enumerate(lst):
-                    x = e[0]
-                    y = dataDic[e[1]]
-                    if (x == 0):
-                        tmp.append(str(self._UTCTimestamp2LocalizedDate(y)))
-                    else:
-                        tmp.append(y)
-                """
             else:
-                #tmp = ["No minute data available for " + self._UTCTimestamp2LocalizedDate(timeStampUTC)]
-                #print(dic)
                 tmp = str(dic['Message'])
         return tmp
 
-    def explore(self):
-        print('\nConverting now ...')
-        fmt = "%Y-%m-%d %H:%M:%S %Z%z"
-
-        print('Current time in UTC')
-        now_utc = datetime.now(timezone('UTC'))
-#        now_utc = datetime.utcnow()
-        print(now_utc.strftime(fmt))
-
-        print('Convert to US/Pacific time zone')
-        now_pacific = now_utc.astimezone(timezone('US/Pacific'))
-        print(now_pacific.strftime(fmt))
-
-        print('Convert to Europe/Zurich time zone')
-        now_berlin = now_pacific.astimezone(timezone('Europe/Zurich'))
-        print(now_berlin.strftime(fmt))
-
-
-        print('\nLocalizing a date/time')
-        dateStr = "2014-05-28 22:28:15"
-        datetimeObj_naive = datetime.strptime(dateStr, "%Y-%m-%d %H:%M:%S")
-
-        # Wrong way!
-        datetimeObj_pacific = datetimeObj_naive.replace(tzinfo=timezone('US/Pacific'))
-        print('WRONG')
-        print(datetimeObj_pacific.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
-
-        # Right way!
-        datetimeObj_pacific = timezone('US/Pacific').localize(datetimeObj_naive)
-        print('RIGHT')
-        print(datetimeObj_pacific.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
-
-        datetimeObj = datetime.strptime(dateStr, "%Y-%m-%d %H:%M:%S")
-        localDatetimeObj = timezone("Europe/Zurich").localize(datetimeObj)
-        print("local time:               " + localDatetimeObj.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
-        datetimeObjUTC = localDatetimeObj.astimezone(timezone('UTC'))
-        timeStampUTC = time.mktime(datetimeObjUTC.timetuple())
-        print("local time to UTC time:   " + datetimeObjUTC.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
-        swissDateTimeObj = datetimeObjUTC.astimezone(timezone("Europe/Zurich"))
-        print("local time from UTC time: " + swissDateTimeObj.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
-
-
-        print('\nConverting UTC date to other time zone')
-        dateStr = "2014-05-28 22:28:15"
-        datetimeObj = datetime.strptime(dateStr, "%Y-%m-%d %H:%M:%S")
-        datetimeObjUTC = datetimeObj.replace(tzinfo=timezone('UTC'))
-        print('Date in UTC')
-        print(datetimeObjUTC.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
-        print('Convert to US/Pacific time zone')
-        pacificTime = datetimeObjUTC.astimezone(timezone('US/Pacific'))
-        print(pacificTime.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
-        print('Convert Europe/Zurich time zone')
-        zurichTime = datetimeObjUTC.astimezone(timezone('Europe/Zurich'))
-        print(zurichTime.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
-
-def play():
-    pr = PriceRequester('Europe/Zurich')
-    #pr.explore()
-
-    """
-    localDateTimeStr = "28-08-17 12:29"
-    datetimeObj = datetime.strptime(localDateTimeStr, DATE_TIME_FORMAT)
-    datetimeObjUTC = datetimeObj.replace(tzinfo=timezone('UTC'))
-    print('Date in UTC wrong')
-    print(datetimeObjUTC.strftime(DATE_TIME_FORMAT_TZ))
-    timeStampUTC = time.mktime(datetimeObjUTC.timetuple())
-    print(timeStampUTC)
-
-
-    datetimeObjUTC = timezone('UTC').localize(datetimeObj)
-    print('Date in UTC right')
-    print(datetimeObjUTC.strftime(DATE_TIME_FORMAT_TZ))
-    timeStampUTC = time.mktime(datetimeObjUTC.timetuple())
-    print(timeStampUTC)
-
-
-    datetimeObjZH = datetimeObjUTC.astimezone(timezone('Europe/Zurich'))
-    print('Date in Europe/Zurich')
-    print(datetimeObjZH.strftime(DATE_TIME_FORMAT_TZ))
-    timeStampZH = time.mktime(datetimeObjZH.timetuple())
-    print(timeStampZH)
-    print(timeStampZH - timeStampUTC)
-    crypto = 'BTC'
-    fiat = 'USD'
-    print(crypto + ' ' + str(pr.getPriceAtTimeStampUTC(crypto, fiat, timeStampUTC, 'CCEX')))
-
-    #timeStampUTC = time.mktime(datetime.utcnow().timetuple())
-    """
-    timeStampUTC = time.mktime(datetime.now(timezone('UTC')).timetuple())
-    crypto = 'BTC'
-    fiat = 'USD'
-    # print(crypto + ' ' + str(pr.getPriceAtTimeStampUTC(crypto, fiat, timeStampUTC, 'CCEX')))
-    crypto = 'MCAP'
-    print(crypto + ' (now tstamp UTC: ' + str(pr.getPriceAtUTCTimeStamp(crypto, fiat, timeStampUTC, 'CCEX')))
-
-    localDateTimeStampNow = time.mktime(datetime.now(timezone('Europe/Zurich')).timetuple())
-    print(crypto + ' (now tstamp Europe/Zurich:NONSENSE ' + str(pr.getPriceAtUTCTimeStamp(crypto, fiat, localDateTimeStampNow, 'CCEX')))
-    localDateTimeStr = datetime.fromtimestamp(localDateTimeStampNow).strftime(DATE_TIME_FORMAT)
-    print(crypto + ' (now date str Europe/Zurich: ' + str(pr.getPriceAtLocalDateTimeStr(crypto, fiat, localDateTimeStr, 'CCEX')))
-
-    localDateTimeStr = "1-9-17 11:00"
-    print(crypto + ' ' + str(pr.getPriceAtLocalDateTimeStr(crypto, fiat, localDateTimeStr, 'CCEX')))
-        
 if __name__ == '__main__':
-    #play()
     import re
     pr = PriceRequester('Europe/Zurich')
-    prompt = "crypto fiat d-m[-y] h:m exch (q/quit): "
+    prompt = "crypto fiat d/m[/y] h:m exch (q/quit):\n"
     inputStr = input(prompt)
-
+    #([0-9]+)-([0-9]+)(?:-([0-9]+)|) matches either 1-9 or 1-9-17
+    patternFullData = r"(\w+) (\w+) ([0-9]+)/([0-9]+)(?:/([0-9]+)|) ([0-9:]+)(?: (\w+)|)"
+    patternPartialData = r"(?:(-\w)([\w\d/:]+))(?: (-\w)([\w\d/:]+))?(?: (-\w)([\w\d/:]+))?(?: (-\w)([\w\d/:]+))?(?: (-\w)([\w\d/:]+))?"
+    crypto = ''
+    fiat = ''
+    exchange = ''
+    day = ''
+    month = ''
+    year = ''
+    hourMin = ''
+    localDateTimeStr = ''
+    
     while inputStr.upper() != 'Q':
-        #([0-9]+)-([0-9]+)(?:-([0-9]+)|) matches either 1-9 or 1-9-17
-        pattern = r"(\w+) (\w+) ([0-9]+)-([0-9]+)(?:-([0-9]+)|) ([0-9:]+)(?: (\w+)|)"
-        data = re.match(pattern, inputStr)
-        crypto = data.group(1).upper()
-        fiat = data.group(2).upper()
-        day = data.group(3)
-        month = data.group(4)
-        year = data.group(5)
-            
-        hourMin = data.group(6)
+        data = re.match(patternFullData, inputStr)
         
-        if year == None:
-            year = '17'
-        elif len(year) > 2:
-            year = year[-2:]
+        if data == None:
+            data = re.match(patternPartialData, inputStr)
+            if data != None:
+                partialArgList = data.groups()
+                i = 0
+                while i < len(partialArgList):
+                    arg = partialArgList[i]
+                    if arg != None:
+                        val = partialArgList[i + 1]
+                        if val != None:
+                            valLower = val #for exchange !
+                            val = val.upper()
+                            arg = arg.upper()
+                            if '-C' == arg:
+                                crypto = val
+                            elif '-F' == arg:
+                                fiat = val
+                            elif '-D' == arg:
+                                date = val
+                            elif '-T' == arg:
+                                hourMin = val
+                            elif '-E' == arg:
+                                exchange = valLower
+                    i += 2
+            else:
+                inputStr = input(prompt)
+                continue
+        else: #regular command line entered         
+            crypto = data.group(1).upper()
+            fiat = data.group(2).upper()
+            day = data.group(3)
+            month = data.group(4)
+            year = data.group(5)
             
-        exchange = data.group(7)
+            hourMin = data.group(6)
         
-        if exchange == None:
-            exchange = 'CCCAGG'
+            if year == None:
+                year = '17'
+            elif len(year) > 2:
+                year = year[-2:]
             
-        localDateTimeStr = day + '-' + month + '-' + year + ' ' + hourMin
+            exchange = data.group(7)
+        
+            if exchange == None:
+                exchange = 'CCCAGG'
+                    
+        if ':' not in hourMin:
+            hourMin = hourMin + ':00'
+    
+        localDateTimeStr = day + '/' + month + '/' + year + ' ' + hourMin
         print("{}/{} on {}: ".format(crypto, fiat, exchange) + ' '.join(map(str, pr.getPriceAtLocalDateTimeStr(crypto, fiat, localDateTimeStr, exchange))))
         inputStr = input(prompt)
