39603ee23698f2445a251eb2671dd88e899346b0
Archanciel <jp.schnyder@gmail.com>
2017-09-14 22:11:42 +0200

    add display time zone

--- essts.py
+++ essts.py
@@ -3,7 +3,7 @@ from pytz import timezone
 import time
 
 def timestamp2date(timestamp):
-    DATE_TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+    DATE_TIME_FORMAT = "%Y-%m-%d %H:%M:%S %Z%z"
 
     # function converts a UTC timestamp into Europe/Zurich Gregorian date
     utcTimeStamp = datetime.fromtimestamp(int(timestamp)).replace(tzinfo=timezone('UTC'))
@@ -11,6 +11,7 @@ def timestamp2date(timestamp):
     return utcTimeStamp.astimezone(timezone('Europe/Zurich')).strftime(DATE_TIME_FORMAT)
 
 timeStampUTC_1 = time.mktime(datetime.utcnow().timetuple())
+print(datetime.utcnow().timetuple())
 print(timeStampUTC_1)
 print(timestamp2date(timeStampUTC_1))
 timeStampUTC_2 = time.mktime(datetime.now(timezone('UTC')).timetuple())
@@ -20,7 +21,7 @@ print(timeStampUTC_2 - timeStampUTC_1)
 
 
 # 1504385450.0
-# 2017-09-03 00:50:50   this the right time
+# 2017-09-03 00:50:50 CEST+0200   this the right time
 # 1504389050.0
-# 2017-09-03 01:50:50
+# 2017-09-03 01:50:50 CEST+0200
 # 3600.0
