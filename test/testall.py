aa4b8790c251de6541054676103851b0619d50ed
Archanciel <jp.schnyder@gmail.com>
2017-09-18 19:28:07 +0200

    add TestController

--- test/testall.py
+++ test/testall.py
@@ -4,13 +4,15 @@ from testrequester import TestRequester
 from testabstractcommand import TestAbstractCommand
 from testcommandquit import TestCommandQuit
 from testcommanderror import TestCommandError
+from testcontroller import TestController
 
 if __name__ == "__main__":
     loader = TestLoader() 
     suite = TestSuite((loader.loadTestsFromTestCase(TestRequester),
                        loader.loadTestsFromTestCase(TestAbstractCommand),
                        loader.loadTestsFromTestCase(TestCommandQuit),
-                       loader.loadTestsFromTestCase(TestCommandError)
-                      ))
+                       loader.loadTestsFromTestCase(TestCommandError),
+                       loader.loadTestsFromTestCase(TestController)
+    ))
     runner = TextTestRunner(verbosity = 2) 
     runner.run(suite)
