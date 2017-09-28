2777c16b096c7eabaab6a2e4432f6736eaf78e19
Archanciel <jp.schnyder@gmail.com>
2017-09-28 21:49:34 +0200

    Renamed so not executed when running all utests of test dir

---
+++ test/all.py
@@ -0,0 +1,20 @@
+from unittest import TestLoader, TextTestRunner, TestSuite 
+
+from testrequester import TestRequester
+from testabstractcommand import TestAbstractCommand
+from testcommandquit import TestCommandQuit
+from testcommanderror import TestCommandError
+from testcontroller import TestController
+from testconfigurationmanager import TestConfigurationManager
+
+if __name__ == "__main__":
+    loader = TestLoader() 
+    suite = TestSuite((loader.loadTestsFromTestCase(TestRequester),
+                       loader.loadTestsFromTestCase(TestAbstractCommand),
+                       loader.loadTestsFromTestCase(TestCommandQuit),
+                       loader.loadTestsFromTestCase(TestCommandError),
+                       loader.loadTestsFromTestCase(TestController),
+                       loader.loadTestsFromTestCase(TestConfigurationManager)
+    ))
+    runner = TextTestRunner(verbosity = 2) 
+    runner.run(suite)
