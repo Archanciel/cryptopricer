8b841297dd33db0a386b422307963d9d5a2442c9
Archanciel <jp.schnyder@gmail.com>
2017-09-29 22:22:23 +0200

    using property en prévision use configobj

--- configurationmanager.py
+++ configurationmanager.py
@@ -1,6 +1,10 @@
 class ConfigurationManager:
     def __init__(self):
-        self.localTimeZone = 'Europe/Zurich'
+        self._localTimeZone = 'Europe/Zurich'
+
+    @property
+    def localTimeZone(self):
+        return self._localTimeZone
 
 if __name__ == '__main__':
     pass
