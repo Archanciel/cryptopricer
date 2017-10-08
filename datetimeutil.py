c99c6c96b906c18c632e3189e88879a6c8081c21
Archanciel <jp.schnyder@gmail.com>
2017-10-08 22:49:09 +0200

    add functionality

--- datetimeutil.py
+++ datetimeutil.py
@@ -4,18 +4,57 @@ class DateTimeUtil:
     @staticmethod
     def timeStampToArrowLocalDate(timeStamp, timeZoneStr):
         '''
-        From timestamp, returns a localized arrow object
+        FGiven a UTC/GMT timezone independent timestamp and a timezone string specification,
+        returns a localized arrow object.
+
         :param timeStamp: UTC/GMT timezone independent timestamp
         :param timeZoneStr: like 'Europe/Zurich' or 'US/Pacific'
-        :return: arrow.Arrow instance
+        :return: arrow localized date time object
         '''
         return arrow.Arrow.utcfromtimestamp(timeStamp).to(timeZoneStr)
 
+
     @staticmethod
-    def dateTimeStringToTimeStamp(dateTimeStr, dateTimeFormatArrow, timeZoneStr):
+    def dateTimeStringToTimeStamp(dateTimeStr, timeZoneStr, dateTimeFormatArrow):
+        '''
+        Given a datetime string which format abide to to the passed arrow format and
+        a timezone string specification, return a UTC/GMT timezone independent timestamp.
+
+        :param dateTimeStr:
+        :param timeZoneStr: like 'Europe/Zurich' or 'US/Pacific'
+        :param dateTimeFormatArrow: example YYYY/MM/DD HH:mm:ss --> 2017/09/12 15:32:21
+        :return: int UTC/GMT timezone independent timestamp
+        '''
         arrowObj = arrow.get(dateTimeStr, dateTimeFormatArrow).replace(tzinfo=timeZoneStr)
 
         return arrowObj.timestamp  # timestamp is independant from timezone !
+
+
+    @staticmethod
+    def dateTimeStringToArrowLocalDate(dateTimeStr, timeZoneStr, dateTimeFormatArrow):
+        '''
+        Given a datetime string which format abide to to the passed arrow format and
+        a timezone string specification, return an arrow localized date time object.
+
+        :param dateTimeStr:
+        :param timeZoneStr: like 'Europe/Zurich' or 'US/Pacific'
+        :param dateTimeFormatArrow: example YYYY/MM/DD HH:mm:ss --> 2017/09/12 15:32:21
+        :return: arrow localized date time object
+        '''
+        return arrow.get(dateTimeStr, dateTimeFormatArrow).replace(tzinfo=timeZoneStr)
+
+
+    @staticmethod
+    def convertToTimeZone(dateTimeArrowObject, timeZoneStr):
+        '''
+        Return the passed dateTimeArrowObject converted to the passed timeZoneStr.
+        The passed dateTimeArrowObject remains unchanged !
+
+        :param dateTimeArrowObject: arrow localized date time object.
+        :param timeZoneStr: like 'Europe/Zurich' or 'US/Pacific'
+        :return: arrow date time object localized  to passed timeZoneStr
+        '''
+        return dateTimeArrowObject.to(timeZoneStr)
 
 
 if __name__ == '__main__':
--- test/testdatetimeutil.py
+++ test/testdatetimeutil.py
@@ -16,22 +16,96 @@ class TestDateTimeUtil(unittest.TestCase):
         pass
 
 
-    def testTimestampToLocalDate(self):
+    def testTimestampToSummerLocalDate(self):
         timeStamp = 1506787315
+        
         arrowObjIN = DateTimeUtil.timeStampToArrowLocalDate(timeStamp, 'Asia/Calcutta')
         self.assertEqual("2017/09/30 21:31:55 +05:30",arrowObjIN.format(DATE_TIME_FORMAT_TZ_ARROW))
+        
+        arrowObjZH = DateTimeUtil.timeStampToArrowLocalDate(timeStamp, 'Europe/Zurich')
+        self.assertEqual("2017/09/30 18:01:55 +02:00",arrowObjZH.format(DATE_TIME_FORMAT_TZ_ARROW))
+
+
+    def testTimestampToWinterLocalDate(self):
+        timeStamp = 1512057715
+        
+        arrowObjIN = DateTimeUtil.timeStampToArrowLocalDate(timeStamp, 'Asia/Calcutta')
+        self.assertEqual("2017/11/30 21:31:55 +05:30",arrowObjIN.format(DATE_TIME_FORMAT_TZ_ARROW))
+        
+        arrowObjZH = DateTimeUtil.timeStampToArrowLocalDate(timeStamp, 'Europe/Zurich')
+        self.assertEqual("2017/11/30 17:01:55 +01:00",arrowObjZH.format(DATE_TIME_FORMAT_TZ_ARROW))
 
 
     def testSummerDateTimeStringToTimeStamp(self):
+        expTimeStamp = 1506787315
+
         dateStr = '2017/09/30 21:31:55'
-        timeStamp = DateTimeUtil.dateTimeStringToTimeStamp(dateStr, DATE_TIME_FORMAT_ARROW, 'Asia/Calcutta')
-        self.assertEqual(1506787315, timeStamp)
+        timeStamp = DateTimeUtil.dateTimeStringToTimeStamp(dateStr, 'Asia/Calcutta', DATE_TIME_FORMAT_ARROW)
+        self.assertEqual(expTimeStamp, timeStamp)
+
+        dateStr = '2017/09/30 18:01:55'
+        timeStamp = DateTimeUtil.dateTimeStringToTimeStamp(dateStr, 'Europe/Zurich', DATE_TIME_FORMAT_ARROW)
+        self.assertEqual(expTimeStamp, timeStamp)
 
 
-    def testWinerDateTimeStringToTimeStamp(self):
-        dateStr = '2017/11/30 21:31:55'
-        timeStamp = DateTimeUtil.dateTimeStringToTimeStamp(dateStr, DATE_TIME_FORMAT_ARROW, 'Asia/Calcutta')
-        self.assertEqual(1512057715, timeStamp)
+    def testWinterDateTimeStringToTimeStamp(self):
+        expTimeStamp = 1512061315
+        
+        dateStr = '2017/11/30 22:31:55'
+        timeStamp = DateTimeUtil.dateTimeStringToTimeStamp(dateStr, 'Asia/Calcutta', DATE_TIME_FORMAT_ARROW)
+        self.assertEqual(expTimeStamp, timeStamp)
+
+        dateStr = '2017/11/30 18:01:55'
+        timeStamp = DateTimeUtil.dateTimeStringToTimeStamp(dateStr, 'Europe/Zurich', DATE_TIME_FORMAT_ARROW)
+        self.assertEqual(expTimeStamp, timeStamp)
+
+
+    def testSummerDateTimeStringToArrowLocalDate(self):
+        expTimeStamp = 1506787315
+
+        dateStr = '2017/09/30 21:31:55'
+        arrowDateObj = DateTimeUtil.dateTimeStringToArrowLocalDate(dateStr, 'Asia/Calcutta', DATE_TIME_FORMAT_ARROW)
+        self.assertEqual(expTimeStamp, arrowDateObj.timestamp)
+
+        dateStr = '2017/09/30 18:01:55'
+        arrowDateObj = DateTimeUtil.dateTimeStringToArrowLocalDate(dateStr, 'Europe/Zurich', DATE_TIME_FORMAT_ARROW)
+        self.assertEqual(expTimeStamp, arrowDateObj.timestamp)
+
+
+    def testWinterDateTimeStringToArrowLocalDate(self):
+        expTimeStamp = 1512061315
+
+        dateStr = '2017/11/30 22:31:55'
+        arrowDateObj = DateTimeUtil.dateTimeStringToArrowLocalDate(dateStr, 'Asia/Calcutta', DATE_TIME_FORMAT_ARROW)
+        self.assertEqual(expTimeStamp, arrowDateObj.timestamp)
+
+        dateStr = '2017/11/30 18:01:55'
+        arrowDateObj = DateTimeUtil.dateTimeStringToArrowLocalDate(dateStr, 'Europe/Zurich', DATE_TIME_FORMAT_ARROW)
+        self.assertEqual(expTimeStamp, arrowDateObj.timestamp)
+
+
+    def testConvertToTimeZoneSummer(self):
+        locDateStr = '2017/09/30 09:00:00'
+        datetimeObjLA = DateTimeUtil.dateTimeStringToArrowLocalDate(locDateStr, 'US/Pacific', DATE_TIME_FORMAT_ARROW)
+        self.assertEqual('2017/09/30 09:00:00 -07:00', datetimeObjLA.format(DATE_TIME_FORMAT_TZ_ARROW))
+
+        datetimeObjZH = DateTimeUtil.convertToTimeZone(datetimeObjLA, 'Europe/Zurich')
+        self.assertEqual('2017/09/30 18:00:00 +02:00', datetimeObjZH.format(DATE_TIME_FORMAT_TZ_ARROW))
+
+        datetimeObjIN = DateTimeUtil.convertToTimeZone(datetimeObjLA, 'Asia/Calcutta')
+        self.assertEqual('2017/09/30 21:30:00 +05:30', datetimeObjIN.format(DATE_TIME_FORMAT_TZ_ARROW))
+
+
+    def testConvertToTimeZoneWinter(self):
+        locDateStr = '2017/11/30 09:00:00'
+        datetimeObjLA = DateTimeUtil.dateTimeStringToArrowLocalDate(locDateStr, 'US/Pacific', DATE_TIME_FORMAT_ARROW)
+        self.assertEqual('2017/11/30 09:00:00 -08:00', datetimeObjLA.format(DATE_TIME_FORMAT_TZ_ARROW))
+
+        datetimeObjZH = DateTimeUtil.convertToTimeZone(datetimeObjLA, 'Europe/Zurich')
+        self.assertEqual('2017/11/30 18:00:00 +01:00', datetimeObjZH.format(DATE_TIME_FORMAT_TZ_ARROW))
+
+        datetimeObjIN = DateTimeUtil.convertToTimeZone(datetimeObjLA, 'Asia/Calcutta')
+        self.assertEqual('2017/11/30 22:30:00 +05:30', datetimeObjIN.format(DATE_TIME_FORMAT_TZ_ARROW))
 
 
 if __name__ == '__main__':
