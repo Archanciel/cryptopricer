66fa557e9c00dc0d2a8e90c5e336e87f2a854f0d
Archanciel <jp.schnyder@gmail.com>
2017-10-22 00:04:44 +0200

    init commit

---
+++ test/testprocessor.py
@@ -0,0 +1,28 @@
+import unittest
+import os, sys, inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+from processor import Processor
+from configurationmanager import ConfigurationManager
+from pricerequester import PriceRequester
+from crypcompexchanges import CrypCompExchanges
+
+
+class TestProcessor(unittest.TestCase):
+    def setUp(self):
+        if os.name == 'posix':
+            FILE_PATH = '/sdcard/cryptopricer.ini'
+        else:
+            FILE_PATH = 'c:\\temp\\cryptopricer.ini'
+
+        self.configMgr = ConfigurationManager(FILE_PATH)
+        self.priceRequester = PriceRequester()
+        self.crypCompExchanges = CrypCompExchanges()
+        self.processor = Processor(self.configMgr, self.priceRequester, self.crypCompExchanges)
+
+
+if __name__ == '__main__':
+    unittest.main()
