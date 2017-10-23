0a8ccf45129c2b7f448ad82fcc9d6ef10beb3d87
Archanciel <jp.schnyder@gmail.com>
2017-10-23 19:42:18 +0200

    connected to processor + trial code

--- commandprice.py
+++ commandprice.py
@@ -16,11 +16,90 @@ class CommandPrice(AbstractCommand):
         super().__init__(receiver, 'CommandPrice', rawParmData, parsedParmData)
 
     def execute(self):
-        self.receiver.getCryptoPrice(self.parsedParmData[self.CRYPTO], \
+        dayStr = self.parsedParmData[self.DAY]
+        if dayStr != None:
+            day = int(dayStr)
+        else:
+            day = 0
+
+        monthStr = self.parsedParmData[self.MONTH]
+        if monthStr != None:
+            month = int(monthStr)
+        else:
+            month = 0
+ 
+        yearStr = self.parsedParmData[self.YEAR]
+        if yearStr != None:
+            year = int(yearStr)
+        else:
+            year = 0
+            
+        hourStr = self.parsedParmData[self.HOUR]
+        if hourStr != None:
+            hour = int(hourStr)
+        else:
+            hour = 0
+
+        minuteStr = self.parsedParmData[self.MINUTE]
+        if minuteStr != None:
+            minute = int(minuteStr)
+        else:
+            minute = 0
+    
+        result = self.receiver.getCryptoPrice(self.parsedParmData[self.CRYPTO], \
         	                            self.parsedParmData[self.FIAT], \
         	                            self.parsedParmData[self.EXCHANGE], \
-        	                            self.parsedParmData[self.DAY], \
-        	                            self.parsedParmData[self.MONTH], \
-        	                            self.parsedParmData[self.YEAR], \
-        	                            self.parsedParmData[self.HOUR], \
-        	                            self.parsedParmData[self.MINUTE])
+        	                            day, \
+        	                            month, \
+        	                            year, \
+        	                            hour, \
+        	                            minute)
+        	                            
+        return result
+
+
+if __name__ == '__main__':
+    from configurationmanager import ConfigurationManager
+    from pricerequester import PriceRequester
+    from crypcompexchanges import CrypCompExchanges
+    from processor import Processor
+    import os
+
+    if os.name == 'posix':
+        FILE_PATH = '/sdcard/cryptopricer.ini'
+    else:
+        FILE_PATH = 'c:\\temp\\cryptopricer.ini'
+
+    cm = ConfigurationManager(FILE_PATH)
+    pr = PriceRequester()
+    cryp = CrypCompExchanges()
+    proc = Processor(cm, pr, cryp)
+    
+    cpr = CommandPrice(proc)
+
+    print('HISTORICAL')
+
+    cpr.parsedParmData[cpr.CRYPTO] = 'BTC'
+    cpr.parsedParmData[cpr.FIAT] = 'USD'
+    cpr.parsedParmData[cpr.EXCHANGE] = 'bittrex'
+    cpr.parsedParmData[cpr.DAY] = '12'
+    cpr.parsedParmData[cpr.MONTH] = '9'
+    cpr.parsedParmData[cpr.YEAR] = '2017'
+    cpr.parsedParmData[cpr.HOUR] = '10'
+    cpr.parsedParmData[cpr.MINUTE] = '5'
+    print(cpr.execute())
+
+    cpr.parsedParmData[cpr.EXCHANGE] = 'unknown'
+    print(cpr.execute())
+
+    print('\nREAL TIME')
+    
+    cpr.parsedParmData[cpr.EXCHANGE] = 'bittrex'
+    cpr.parsedParmData[cpr.DAY] = '0'
+    cpr.parsedParmData[cpr.MONTH] = '0'
+    cpr.parsedParmData[cpr.YEAR] = '0'
+
+    print(cpr.execute())
+
+    cpr.parsedParmData[cpr.EXCHANGE] = 'unknown'
+    print(cpr.execute())
