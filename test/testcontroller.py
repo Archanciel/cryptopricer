c5cb317511fb9178a7ac6890e49ae783b9c7e45d
Archanciel <jp.schnyder@gmail.com>
2017-10-25 00:56:05 +0200

    first attempt

--- test/testcontroller.py
+++ test/testcontroller.py
@@ -12,10 +12,39 @@ from commandcrypto import CommandCrypto
 from commandquit import CommandQuit
 from commanderror import CommandError
 
-
+'''
+GBYTE/USD on Bittrex:  ERROR - e param is not valid the market does not exist for this coin pair
+Enter command (h for help, q to quit)
+BTC/USD on BitTrex:  25/10/17 00:39 5499.9
+Enter command (h for help, q to quit)
+GBYTE/BTC on BitTrex:  25/10/17 00:39 0.03331
+Enter command (h for help, q to quit)
+ERROR - Unknown market does not exist for this coin pair (BTC-USD)
+Enter command (h for help, q to quit)
+ERROR - exchange name does not respect required format: must start with an upper case
+Enter command (h for help, q to quit)
+Quit ? y/n 
+'''
 class TestController(unittest.TestCase):
     def setUp(self):
         self.controller = Controller()
+
+
+    # def testRunGetHistoMinutePrice(self):
+    #     stdin = sys.stdin
+    #     sys.stdin = StringIO('btc usd 24/10/2017 22:33 Bittrex' + \
+    #                          '\ngbyte btc 24/10/2017 22:33 Bittrex' + \
+    #                          '\ngbyte usd 24/10/2017 22:33 Bittrex' + \
+    #                          '\nbtc usd 0 Bittrex' + \
+    #                          '\ngbyte btc 0 Bittrex' + \
+    #                          '\nbtc usd 12/10/2017 12:00 Unknown' + \
+    #                          '\nbtc usd 12/10/2017 12:00 bittrex\nq\ny')
+    #
+    #     self.controller.run()
+    #
+    #     #capture stdout for your assert !
+    #
+    #     sys.stdin = stdin
 
 
 if __name__ == '__main__':
