e9adcdf0a9f20d23c16970de7c061b0e9c0d1f77
Archanciel <jp.schnyder@gmail.com>
2017-12-11 23:35:44 +0100

    add comments

--- maincl.py
+++ maincl.py
@@ -4,6 +4,8 @@ from consoleoutputformater import ConsoleOutputFormater
 def main():
     '''
     Maincl means main command line !
+    Command line main which instanciate a Controller which uses a ConsoleOutputFormater
+    instead of a GuiOutputFormater, what maingui does !
     '''
     c = Controller(ConsoleOutputFormater())
     c.run()
--- maingui.py
+++ maingui.py
@@ -2,6 +2,11 @@ from controller import Controller
 from guioutputformater import GuiOutputFormater
 
 def main():
+    '''
+    Command line main which instanciate a Controller which uses a GuiOutputFormater
+    instead of a ConsoleOutputFormater, what maincl does !
+    :return:
+    '''
     c = Controller(GuiOutputFormater())
     c.run()
 
