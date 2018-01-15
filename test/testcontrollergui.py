863007461f7f2624eaa27eb382269d2b887d07d7
Archanciel <jp.schnyder@gmail.com>
2018-01-15 23:14:26 +0100

    unit test scenario for testGetPrintableResultForInput …¹G

--- test/testcontrollergui.py
+++ test/testcontrollergui.py
@@ -5,6 +5,7 @@ from io import StringIO
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
+sys.path.insert(0,currentdir) #this instruction is necessary for successful importation of testcontroller module when TestControllerGui is executed standalone
 
 import re
 from controller import Controller
@@ -58,6 +59,124 @@ class TestControllerGui(TestController):
             contentList = inFile.readlines()
             self.assertEqual('BTC/USD on CCCAGG: ' + '{}/{}/{} {}:{}R'.format(nowDayStr, nowMonthStr, now.year - 2000, nowHourStr, nowMinuteStr), self.removePriceFromResult(contentList[1][:-1])) #removing \n from contentList entry !
             self.assertEqual('BTC/USD on CCCAGG: ' + '{}/{}/{} {}:{}R'.format(nowDayStr, nowMonthStr, now.year - 2000, nowHourStr, nowMinuteStr), self.removePriceFromResult(contentList[3][:-1])) #removing \n from contentList entry !
+
+
+        now = DateTimeUtil.localNow('Europe/Zurich')
+        yesterday = now.shift(days=-2)
+        yesterdayDay = yesterday.day
+        yesterdayMonth = yesterday.month
+
+        nowMonthStr, nowDayStr, nowHourStr, nowMinuteStr = self.getFormattedDateTimeComponentsForArrowDateTimeObj(now)
+
+        yesterdayMinute = yesterday.minute
+
+        if yesterdayMinute < 10:
+            if yesterdayMinute > 0:
+                yesterdayMinuteStr = '0' + str(yesterdayMinute)
+            else:
+                yesterdayMinuteStr = '00'
+        else:
+            yesterdayMinuteStr = str(yesterdayMinute)
+
+        yesterdayHour = yesterday.hour
+
+        if yesterdayHour < 10:
+            if yesterdayHour > 0:
+                yesterdayHourStr = '0' + str(yesterdayHour)
+            else:
+                yesterdayHourStr = '00'
+        else:
+            yesterdayHourStr = str(yesterdayHour)
+
+        yesterdayDay = yesterday.day
+
+        if yesterdayDay < 10:
+            yesterdayDayStr = '0' + str(yesterdayDay)
+        else:
+            yesterdayDayStr = str(yesterdayDay)
+
+        yesterdayMonth = yesterday.month
+
+        if yesterdayMonth < 10:
+            yesterdayMonthStr = '0' + str(yesterdayMonth)
+        else:
+            yesterdayMonthStr = str(yesterdayMonth)
+
+        #first command: RT price command
+        inputStr = 'eth usd 0 bitfinex'
+        printResult, fullCommandStr, fullCommandStrWithSaveModeOptions = self.controller.getPrintableResultForInput(
+            inputStr)
+
+        self.assertEqual(
+            'ETH/USD on Bitfinex: ' + '{}/{}/{} {}:{}R'.format(nowDayStr, nowMonthStr, now.year - 2000, nowHourStr,
+                                                               nowMinuteStr),
+            self.removePriceFromResult(printResult))
+        self.assertEqual('eth usd 0 bitfinex', fullCommandStr)
+        self.assertEqual(None, fullCommandStrWithSaveModeOptions)
+
+        #second command: value command
+        inputStr = '-v10eth'
+        printResult, fullCommandStr, fullCommandStrWithSaveModeOptions = self.controller.getPrintableResultForInput(
+            inputStr)
+        self.assertEqual(
+            'ETH/USD on Bitfinex: ' + '{}/{}/{} {}:{}R'.format(nowDayStr, nowMonthStr, now.year - 2000, nowHourStr,
+                                                               nowMinuteStr),
+            self.removePricesFromCommandValueResult(printResult))
+        self.assertEqual('eth usd 0 bitfinex', fullCommandStr)
+        self.assertEqual(None, fullCommandStrWithSaveModeOptions)
+
+        #third command: value save command
+        inputStr = '-vs100usd'
+        printResult, fullCommandStr, fullCommandStrWithSaveModeOptions = self.controller.getPrintableResultForInput(
+            inputStr)
+        self.assertEqual(
+            'ETH/USD on Bitfinex: ' + '{}/{}/{} {}:{}R'.format(nowDayStr, nowMonthStr, now.year - 2000, nowHourStr,
+                                                               nowMinuteStr),
+            self.removePricesFromCommandValueResult(printResult))
+        self.assertEqual('eth usd 0 bitfinex', fullCommandStr)
+        self.assertEqual('eth usd 0 bitfinex -vs100usd', fullCommandStrWithSaveModeOptions)
+
+        #fourth command: '' to replay lst command
+        inputStr = ''
+        printResult, fullCommandStr, fullCommandStrWithSaveModeOptions = self.controller.getPrintableResultForInput(
+            inputStr)
+        self.assertEqual(
+            'ETH/USD on Bitfinex: ' + '{}/{}/{} {}:{}R'.format(nowDayStr, nowMonthStr, now.year - 2000, nowHourStr,
+                                                               nowMinuteStr),
+            self.removePricesFromCommandValueResult(printResult))
+        self.assertEqual('eth usd 0 bitfinex', fullCommandStr)
+        self.assertEqual('eth usd 0 bitfinex -vs100usd', fullCommandStrWithSaveModeOptions)
+
+        #fifth command: change crypto
+        inputStr = '-cneo'
+        printResult, fullCommandStr, fullCommandStrWithSaveModeOptions = self.controller.getPrintableResultForInput(
+            inputStr)
+        self.assertEqual(
+            'NEO/USD on Bitfinex: ' + '{}/{}/{} {}:{}R'.format(nowDayStr, nowMonthStr, now.year - 2000, nowHourStr,
+                                                               nowMinuteStr),
+            self.removePricesFromCommandValueResult(printResult))
+        self.assertEqual('neo usd 0 bitfinex', fullCommandStr)
+        self.assertEqual('neo usd 0 bitfinex -vs100usd', fullCommandStrWithSaveModeOptions)
+
+        #sixth command: remove value command
+        inputStr = '-v0'
+        printResult, fullCommandStr, fullCommandStrWithSaveModeOptions = self.controller.getPrintableResultForInput(
+            inputStr)
+        self.assertEqual(
+            'NEO/USD on Bitfinex: ' + '{}/{}/{} {}:{}R'.format(nowDayStr, nowMonthStr, now.year - 2000, nowHourStr,
+                                                               nowMinuteStr),
+            self.removePriceFromResult(printResult))
+        self.assertEqual('neo usd 0 bitfinex', fullCommandStr)
+        self.assertEqual(None, fullCommandStrWithSaveModeOptions)
+
+
+    def removePricesFromCommandValueResult(self, resultStr):
+        match = re.match(r"(?:[\d\.]*) (\w*/)(?:[\d\.]*) (.*) (?:[\d\.]*)", resultStr)
+
+        if match != None:
+            return match.group(1) + match.group(2)
+        else:
+            return ()
 
 
 if __name__ == '__main__':
