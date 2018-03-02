9ddd4bfe7685cb9a305cd30eac2895ff79fec9ef
Archanciel <jp.schnyder@gmail.com>
2018-03-02 22:33:08 +0100

    improv stack info extraction & exploxring inspect method

--- callstackrecorder.py
+++ callstackrecorder.py
@@ -1,4 +1,4 @@
-import traceback, re
+import traceback, re, ast, inspect
 
 class CallStackRecorder:
     '''
@@ -22,6 +22,35 @@ class CallStackRecorder:
         frameList = re.findall(r"(?:<FrameSummary file ([\w:\\,._\s]+)(?:>, |>\]))", frameListLine)
         if frameList:
             with open("c:\\temp\\stack.txt", "a") as f:
-                for frame in frameList[:-1]: #last line is the call to this method !
-                    f.write(frame + '\n')
+                for frame in frameList[:-1]: #last line in frameList is the call to this method !
+                    match = re.match(r"([\w:\\,.]+), line \d* in (.*)", frame)
+                    if match:
+                        pythonFilePathName = match.group(1)
+                        methodName = match.group(2)
+                        with open(pythonFilePathName, "r") as sourceFile:
+                            source = sourceFile.read()
+                            p = ast.parse(source)
+                            classes = [node.name for node in ast.walk(p) if isinstance(node, ast.ClassDef)]
+                            f.write(classes[0] + '.' + methodName + '\n')
+
+if __name__ == '__main__':
+    import importlib
+    module = importlib.import_module('controller')
+    class_ = getattr(module, 'Controller')
+    print(class_.__doc__)
+    instance = None
+    noneStr = ''
+
+    try:
+        instance = eval('class_('+ noneStr + ')')
+    except TypeError:
+        noneStr = 'None'
+        while not instance:
+            try:
+                instance = eval('class_('+ noneStr + ')')
+            except TypeError:
+                noneStr += ', None'
+
+    methods = inspect.getmembers(instance, predicate=inspect.ismethod)
+    print(instance.__class__)
 
