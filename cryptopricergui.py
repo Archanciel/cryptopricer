c2c109cbfa668c005dfc1e01988782996b51539b
Archanciel <jp.schnyder@gmail.com>
2019-07-23 20:35:34 +0200

    cosmetic chg

--- cryptopricergui.py
+++ cryptopricergui.py
@@ -510,7 +510,7 @@ class CryptoPricerGUI(BoxLayout):
 
     def displayHelp(self):
         self.dropDownMenu.dismiss()
-        popup = Popup(title='CryptoPricer', content=Label(text='Version 2.2'), size_hint=(None, None), size=(400, 400))
+        popup = Popup(title='CryptoPricer HELP', content=Label(text='Version 2.2'), size_hint=(None, None), size=(400, 400))
         popup.open()
 
     def updateStatusBar(self, messageStr):
