3fcc8917544c783598c5c9cb06eec8af1e777dc6
Archanciel <jp.schnyder@gmail.com>
2019-07-31 18:39:30 +0200

    init commit

---
+++ encodeTab.py
@@ -0,0 +1,62 @@
+def encodeTabbedLines():
+    encodedLinesList = []
+    tabMode = False
+       
+    with open('/sdcard/tabbedText.txt', 'r') as file:
+        for line in file.read().splitlines():
+            if '    ' in line:
+                tabMode = True
+                line = line.replace('    ', '[t]')
+            else:
+                if tabMode:
+                    line = '[/t]' + line
+                    tabMode = False
+                   
+            encodedLinesList.append(line)
+
+    return '\n'.join(encodedLinesList)
+
+def encodeTabbedLinesVariant():
+    encodedLinesList = []
+    tabMode = False
+       
+    with open('/sdcard/tabbedText.txt', 'r') as file:
+        for line in file.read().splitlines():
+            if '    ' in line:
+                tabMode = True
+                line = line.replace('    ', '[t]')
+            else:
+                if tabMode:
+                    #line = '[/t]' + line
+                    tabMode = False
+                   
+            encodedLinesList.append(line)
+
+    return '\n'.join(encodedLinesList)
+
+def encodeTabbedLinesSmarter():
+    encodedLinesList = []
+    tabMode = False
+       
+    with open('/sdcard/tabbedText.txt', 'r') as file:
+        for line in file.read().splitlines():
+            if '    ' in line and not tabMode:
+                tabMode = True
+                line = line.replace('    ', '[t]')
+            else:
+                if not '    ' in line and tabMode:
+                    line = '[/t]' + line
+                    tabMode = False
+                else:
+                    line = line.replace('    ', '')
+                    
+            encodedLinesList.append(line)
+
+    return '\n'.join(encodedLinesList)
+
+print('\ninitial\n')
+print(encodeTabbedLines())
+print('\nvariant\n')
+print(encodeTabbedLinesVariant())
+print('\nsmarter\n')
+print(encodeTabbedLinesSmarter())
