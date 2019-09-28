0c2c73e47a522d9ee3674fdc63aa402943574031
Archanciel <jp.schnyder@gmail.com>
2019-09-28 19:16:34 +0200

    solved fiat/unit not supported by exchange error. all tsts ok !

--- processor.py
+++ processor.py
@@ -347,7 +347,8 @@ class Processor:
 
         if not fiatResultData.isError():
             fiatConversionRate = fiatResultData.getValue(resultData.RESULT_KEY_PRICE)
-            fiatConvertedPrice = resultData.getValue(resultData.RESULT_KEY_PRICE) * fiatConversionRate
+            price = resultData.getValue(resultData.RESULT_KEY_PRICE)
+            fiatConvertedPrice = price * fiatConversionRate
 
             resultData.setValue(resultData.RESULT_KEY_OPTION_FIAT_COMPUTED_AMOUNT, round(fiatConvertedPrice, FIAT_RATE_PRECISION))
             resultData.setValue(resultData.RESULT_KEY_OPTION_FIAT_SYMBOL, fiat)
@@ -362,7 +363,37 @@ class Processor:
 
             return resultData
         else:
-            return fiatResultData
+            # since fiat/unit is not supported by the exchange, we try to request the unit/fiat inverted rate
+            fiatResultData = self._getPrice(fiat,
+                                            unit,
+                                            fiatExchange,
+                                            year,
+                                            month,
+                                            day,
+                                            hour,
+                                            minute,
+                                            dateTimeFormat,
+                                            localTz)
+            if not fiatResultData.isError():
+                fiatConversionRate = fiatResultData.getValue(resultData.RESULT_KEY_PRICE)
+                price = resultData.getValue(resultData.RESULT_KEY_PRICE)
+                fiatConvertedPrice = price * fiatConversionRate
+
+                resultData.setValue(resultData.RESULT_KEY_OPTION_FIAT_COMPUTED_AMOUNT,
+                                    round(fiatConvertedPrice, FIAT_RATE_PRECISION))
+                resultData.setValue(resultData.RESULT_KEY_OPTION_FIAT_SYMBOL, fiat)
+                resultData.setValue(resultData.RESULT_KEY_OPTION_FIAT_EXCHANGE, fiatExchange)
+
+                unitValuePrice = resultData.getValue(resultData.RESULT_KEY_OPTION_VALUE_UNIT)
+
+                if unitValuePrice:
+                    fiatConvertedUnitValuePrice = unitValuePrice * fiatConversionRate
+                    resultData.setValue(resultData.RESULT_KEY_OPTION_VALUE_FIAT, round(fiatConvertedUnitValuePrice,
+                                                                                       FIAT_RATE_PRECISION))
+
+                return resultData
+            else:
+                return fiatResultData
 
 if __name__ == '__main__':
     from configurationmanager import ConfigurationManager
