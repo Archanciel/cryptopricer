10b54c53455b1af92cfb0da9df4b514334817db3
Archanciel <jp.schnyder@gmail.com>
2020-08-16 16:54:52 +0200

    improved no internet error msg

--- pricerequester.py
+++ pricerequester.py
@@ -181,7 +181,7 @@ class PriceRequester:
         except HTTPError as e:
             resultData.setValue(ResultData.RESULT_KEY_ERROR_MSG, 'ERROR - could not complete request ' + url + '. Reason: ' + str(e.reason))
         except URLError as e:
-            resultData.setValue(ResultData.RESULT_KEY_ERROR_MSG, 'ERROR - could not complete request ' + url + '. Reason: ' + str(e.reason))
+            resultData.setValue(ResultData.RESULT_KEY_ERROR_MSG, 'ERROR - No internet. Please connect and retry !')
         except: 
             the_type, the_value, the_traceback = sys.exc_info()
             resultData.setValue(ResultData.RESULT_KEY_ERROR_MSG, 'ERROR - could not complete request ' + url + '. Reason: ' + str(the_type))
