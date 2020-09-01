ea8aeca39b53cbdf8c42d9641c254207fbb178f2
Archanciel <jp.schnyder@gmail.com>
2020-09-01 17:24:07 +0200

    Update ratedictionary.py

--- ratedictionary.py
+++ ratedictionary.py
@@ -1,22 +1,31 @@
-import json
+import json, os
 
 class RateDictionary:
 	def __init__(self):
+		self.dicFilePath = "/storage/emulated/0/CryptoPricerData/text.txt"
 		self.dic = {}
+
+		
+		if os.path.isfile(self.dicFilePath):
+			with open(self.dicFilePath,'r') as f:
+				self.dic = json.load(f)
 		
 	def getRate(self, crypto, unit, timeStampUTCStr, exchange):
-		pass
+		return self.dic[self.getDicKey(crypto, unit, timeStampUTCStr, exchange)]
 		
 	def saveRate(self, crypto, unit, timeStampUTCStr, exchange, rate):
-		self.dic[crypto + unit + timeStampUTCStr + exchange] = rate
+		self.dic[self.getDicKey(crypto, unit, timeStampUTCStr, exchange)] = rate
 		
 	def saveDic(self):
-		with open("/storage/emulated/0/CryptoPricerData/text.txt",'w') as f:
+		with open(self.dicFilePath,'w') as f:
 			json.dump(self.dic, 
 					  f, 
 					  indent=4,
 					  sort_keys=True)	
 
+	def getDicKey(self, crypto, unit, timeStampUTCStr, exchange):
+		return crypto + unit + timeStampUTCStr + exchange
+		
 if __name__ == "__main__":
 #	d = {"one":1, "two":2}
 #	json.dump(d, open("/storage/emulated/0/CryptoPricerData/text.txt",'w'))
@@ -26,6 +35,7 @@ if __name__ == "__main__":
 #	print(d2["one"])	
 
 	rd = RateDictionary()
-	rd.saveRate('btc', 'usd', '15234867', 'binance', 11000.8542)
-	rd.saveRate('btc', 'eur', '15234847', 'binance', 10500.42)
-	rd.saveDic()		
+	rd.saveRate('btc', 'chf', '15234867', 'binance', 11000.8542)
+	rd.saveRate('btc', 'eth', '15234847', 'binance', 105.42)
+	rd.saveDic()
+	print(rd.getRate('btc', 'eth', '15234847', 'binance'))		
