7f9613a8c7d889cab79e6c97e7d6dd90a5fd978b
Archanciel <jp.schnyder@gmail.com>
2020-12-02 22:18:59 +0100

    improv Status > menu item

--- cryptopricergui.py
+++ cryptopricergui.py
@@ -289,6 +289,7 @@ class CustomDropDown(DropDown):
 		
 		self.owner.requestInput.text = statusBarStr.replace(STATUS_BAR_ERROR_SUFFIX, '')
 		self.owner.statusBarTextInput.text = ''
+		self.statusToRequestInputButton.disabled = True
 
 class ScrollablePopup(Popup):
 	contentBox = ObjectProperty()
