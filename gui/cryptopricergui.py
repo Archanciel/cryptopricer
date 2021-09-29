4b0b0f2c9d141043a629a30613068303b80e2369
Archanciel <jp.schnyder@gmail.com>
2021-09-29 21:04:59 +0200

    Solving awfull Android problem

--- gui/cryptopricergui.py
+++ gui/cryptopricergui.py
@@ -947,9 +947,12 @@ class CryptoPricerGUIApp(App):
 			Config.set('graphics', 'height', '500')
 			Config.write()
 			
-		# avoiding red dot put on Kivy screen after mouse right-click
-		Config.set('input', 'mouse', 'mouse,disable_multitouch')
-	
+			# avoiding red dot put on Kivy screen after mouse right-click
+			# WARNING: on Android, this makes impossible to open the history
+			# list as well as causing a kvy.uix.WidgetEception when trying
+			# to open the CustomDropdown menu !
+			Config.set('input', 'mouse', 'mouse,disable_multitouch')
+			
 		self.title = 'CryptoPricer GUI'
 		self.cryptoPricerGUI = CryptoPricerGUI()
 
